4bb3835b560d863560a2908e2e1b18a8fc2a1c86
vixkkyy <mrvickkyy@gmail.com>
2025-12-30 18:41:30 +0530

    authentication

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ApplicationRecord
+  validates :name, presence: true 
+  validates :release_date, presence: true
+  validates :director, presence: true 
+  validates :actor, presence: true 
+
 end
